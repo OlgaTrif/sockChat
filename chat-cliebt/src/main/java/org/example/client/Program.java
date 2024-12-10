f47d48864bfc3a315131e928a65c23e281af8fb0
trifonova_o_e <myowl@list.ru>
2024-12-10 10:23:59 +0300

    1

--- chat-cliebt/src/main/java/org/example/client/Program.java
+++ chat-cliebt/src/main/java/org/example/client/Program.java
@@ -1,8 +1,36 @@
 package org.example.client;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 public class Program {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("ВВедите ваше имя: ");
+            String name = scanner.nextLine();
+            Socket socket = new Socket("localhost", 1400);
+
+            Client client = new Client(socket, name);
+            InetAddress inetAddress = socket.getInetAddress();
+            System.out.println("Inet Address " + inetAddress);
+            String remoteIp = inetAddress.getHostAddress();
+            System.out.println("Remote IP" + remoteIp);
+            System.out.println("Local Port" + socket.getLocalPort());
+
+            client.listenMessage();
+            client.sendMessage();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } {
+
+        }
+
     }
 }
--- chat-server/src/main/java/org/example/server/Program.java
+++ chat-server/src/main/java/org/example/server/Program.java
@@ -3,6 +3,6 @@ package org.example.server;
 public class Program {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Start server...");
     }
 }
